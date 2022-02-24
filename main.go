73a0d5529616645d05578eed49e581c0703a9d9f
Anders Pitman <tapitman11@gmail.com>
2022-02-24 11:28:04 -0700

    Add ability to listen on local port

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"sync"
 )
@@ -12,11 +13,38 @@ import (
 func main() {
 
 	server := flag.String("server", "", "TLS Server")
+	port := flag.Int("port", 0, "Local port to bind to")
 	flag.Parse()
 
-	fmt.Fprintf(os.Stderr, "tuntls connecting to server: %s\n", *server)
+	if *port == 0 {
+		// one-time tunnel over stdin/stdout
+		doTunnel(*server, os.Stdin, os.Stdout)
+	} else {
+		// listen on a port and create tunnels for each connection
+		fmt.Fprintf(os.Stderr, "Listening on port %d\n", *port)
+		listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 
-	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", *server), &tls.Config{
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+
+			go doTunnel(*server, conn, conn)
+		}
+	}
+
+}
+
+func doTunnel(server string, in io.Reader, out io.Writer) {
+	fmt.Fprintf(os.Stderr, "tuntls connecting to server: %s\n", server)
+
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", server), &tls.Config{
 		//RootCAs: roots,
 	})
 	if err != nil {
@@ -28,12 +56,12 @@ func main() {
 	wg.Add(2)
 
 	go func() {
-		io.Copy(conn, os.Stdin)
+		io.Copy(conn, in)
 		wg.Done()
 	}()
 
 	go func() {
-		io.Copy(os.Stdout, conn)
+		io.Copy(out, conn)
 		wg.Done()
 	}()
 
